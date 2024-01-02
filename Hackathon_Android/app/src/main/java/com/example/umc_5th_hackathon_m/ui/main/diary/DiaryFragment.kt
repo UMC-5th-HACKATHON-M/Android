0723bdf89aa83a4f50dd0567b9762bd4edd7adc4
Hyejeong <myejeong0211@gmail.com>
2024-01-02 20:17:46 +0900

    ✨ Feat: 다이어리 화면 구현

--- Hackathon_Android/app/src/main/java/com/example/umc_5th_hackathon_m/ui/main/diary/DiaryFragment.kt
+++ Hackathon_Android/app/src/main/java/com/example/umc_5th_hackathon_m/ui/main/diary/DiaryFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.umc_5th_hackathon_m.databinding.FragmentDiaryBinding
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 
 class DiaryFragment : Fragment() {
     private lateinit var binding : FragmentDiaryBinding
@@ -16,6 +18,18 @@ class DiaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDiaryBinding.inflate(inflater, container, false)
+        calendar()
         return binding.root
     }
+
+    private fun calendar() {
+        val materialCalendarView: MaterialCalendarView = binding.diaryCalendarview
+
+        materialCalendarView.setOnDateChangedListener { widget, date, selected ->
+            binding.diaryQuestionTv.visibility = View.VISIBLE
+            binding.diaryCategoryCv.visibility = View.VISIBLE
+        }
+    }
+
+
 }
