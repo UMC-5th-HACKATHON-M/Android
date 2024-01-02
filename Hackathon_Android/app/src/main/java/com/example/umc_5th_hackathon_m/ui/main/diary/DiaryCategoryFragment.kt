05c8adf6783bd2b3e02a3f36eb0a2ca684b37980
Hyejeong <myejeong0211@gmail.com>
2024-01-02 21:46:10 +0900

    ✨ Feat: 다이어리 카테고리 구현

--- Hackathon_Android/app/src/main/java/com/example/umc_5th_hackathon_m/ui/main/diary/DiaryCategoryFragment.kt
+++ Hackathon_Android/app/src/main/java/com/example/umc_5th_hackathon_m/ui/main/diary/DiaryCategoryFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.umc_5th_hackathon_m.R
 import com.example.umc_5th_hackathon_m.databinding.FragmentDiaryCategoryBinding
+import com.example.umc_5th_hackathon_m.ui.main.MainActivity
 
 class DiaryCategoryFragment : Fragment() {
     private lateinit var binding : FragmentDiaryCategoryBinding
@@ -16,6 +18,17 @@ class DiaryCategoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDiaryCategoryBinding.inflate(inflater, container, false)
+
+        clickListener()
+
         return binding.root
+    }
+
+    private fun clickListener() {
+        binding.categoryBackIv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, DiaryFragment())
+                .commitAllowingStateLoss()
+        }
     }
 }
--- Hackathon_Android/app/src/main/java/com/example/umc_5th_hackathon_m/ui/main/diary/DiaryFragment.kt
+++ Hackathon_Android/app/src/main/java/com/example/umc_5th_hackathon_m/ui/main/diary/DiaryFragment.kt
@@ -1,13 +1,17 @@
 package com.example.umc_5th_hackathon_m.ui.main.diary
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.umc_5th_hackathon_m.R
 import com.example.umc_5th_hackathon_m.databinding.FragmentDiaryBinding
+import com.example.umc_5th_hackathon_m.ui.main.MainActivity
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import java.util.Calendar
 
 class DiaryFragment : Fragment() {
     private lateinit var binding : FragmentDiaryBinding
@@ -18,7 +22,10 @@ class DiaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDiaryBinding.inflate(inflater, container, false)
+
         calendar()
+        clickListener()
+
         return binding.root
     }
 
@@ -28,6 +35,19 @@ class DiaryFragment : Fragment() {
         materialCalendarView.setOnDateChangedListener { widget, date, selected ->
             binding.diaryQuestionTv.visibility = View.VISIBLE
             binding.diaryCategoryCv.visibility = View.VISIBLE
+            val selectedDate: Calendar = Calendar.getInstance()
+            selectedDate.set(date.year, date.month, date.day)
+
+            // 선택한 년도, 월, 일
+            Log.d("Selected Date", "${selectedDate.get(Calendar.YEAR)}-${selectedDate.get(Calendar.MONTH)}-${selectedDate.get(Calendar.DAY_OF_MONTH)}")
+        }
+    }
+
+    private fun clickListener() {
+        binding.diaryCategoryCv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, DiaryCategoryFragment())
+                .commitAllowingStateLoss()
         }
     }
 
